854cbc7a133d72b67c074517b30cb07bcaba170f
204g1a0556 <94549018+204g1a0556@users.noreply.github.com>
2022-08-30 11:35:49 +0530

    V Meghana Created Sample.C
    V Meghana Completed Sample.C with c source code

---
+++ Sample.C
@@ -0,0 +1,11 @@
+/* Name of the Candidate: V.Meghana
+  Roll No:204g1a0556
+  Title of the Experiment: cpu Scheduling Using FCFS Algorithm
+  Date of creation: 30-08-2022
+  Date of Execution: 30-08-2022
+*/
+// Source Code
+#include <stdio.h>
+   int main(){
+    printf("Hello World\n");
+}
